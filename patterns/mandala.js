2a69d175a5f7a77c428abe0bb839a3a28f7b9200
NenadVUkojevic <nenad.vukojevic@gmail.com>
2024-10-13 12:31:20 +0200

    bezier curves, borders

---
+++ patterns/mandala.js
@@ -0,0 +1,45 @@
+const elements = [
+    // inner
+    { sX: 466, sY: 369, eX: 427, eY: 427, cp1X: 458, cp1Y: 406, cp2X: 437, cp2Y: 392 },
+    { sX: 369, sY: 466, eX: 300, eY: 480, cp1X: 337, cp1Y: 486, cp2X: 332, cp2Y: 462 },
+    { sX: 231, sY: 466, eX: 173, eY: 427, cp1X: 194, cp1Y: 458, cp2X: 208, cp2Y: 437 },
+    { sX: 134, sY: 369, eX: 120, eY: 300, cp1X: 114, cp1Y: 337, cp2X: 138, cp2Y: 332 },
+    { sX: 134, sY: 231, eX: 173, eY: 173, cp1X: 142, cp1Y: 194, cp2X: 163, cp2Y: 208 },
+    { sX: 231, sY: 134, eX: 300, eY: 120, cp1X: 263, cp1Y: 114, cp2X: 268, cp2Y: 138 },
+    { sX: 369, sY: 134, eX: 427, eY: 173, cp1X: 406, cp1Y: 142, cp2X: 392, cp2Y: 163 },
+    { sX: 466, sY: 231, eX: 480, eY: 300, cp1X: 486, cp1Y: 263, cp2X: 462, cp2Y: 268 },
+
+
+    { sX: 427, sY: 427, eX: 369, eY: 466, cp1X: 392, cp1Y: 437, cp2X: 406, cp2Y: 458 },
+    { sX: 300, sY: 480, eX: 231, eY: 466, cp1X: 268, cp1Y: 462, cp2X: 263, cp2Y: 486 },
+    { sX: 173, sY: 427, eX: 134, eY: 369, cp1X: 163, cp1Y: 392, cp2X: 142, cp2Y: 406 },
+    { sX: 120, sY: 300, eX: 134, eY: 231, cp1X: 138, cp1Y: 268, cp2X: 114, cp2Y: 263 },
+    { sX: 173, sY: 173, eX: 231, eY: 134, cp1X: 208, cp1Y: 163, cp2X: 194, cp2Y: 142 },
+    { sX: 300, sY: 120, eX: 369, eY: 134, cp1X: 332, cp1Y: 138, cp2X: 337, cp2Y: 114 },
+    { sX: 427, sY: 173, eX: 466, eY: 231, cp1X: 437, cp1Y: 208, cp2X: 458, cp2Y: 194 },
+    { sX: 480, sY: 300, eX: 466, eY: 369, cp1X: 462, cp1Y: 332, cp2X: 486, cp2Y: 337 },
+
+
+    // outer 
+
+    { sX: 485, sY: 377, eX: 441, eY: 441, cp1X: 475, cp1Y: 417, cp2X: 454, cp2Y: 403 },
+    { sX: 377, sY: 485, eX: 300, eY: 500, cp1X: 341, cp1Y: 506, cp2X: 336, cp2Y: 481 },
+    { sX: 223, sY: 485, eX: 159, eY: 441, cp1X: 183, cp1Y: 475, cp2X: 197, cp2Y: 454 },
+    { sX: 115, sY: 377, eX: 100, eY: 300, cp1X: 94, cp1Y: 341, cp2X: 119, cp2Y: 336 },
+    { sX: 115, sY: 223, eX: 159, eY: 159, cp1X: 125, cp1Y: 183, cp2X: 146, cp2Y: 197 },
+    { sX: 223, sY: 115, eX: 300, eY: 100, cp1X: 259, cp1Y: 94, cp2X: 264, cp2Y: 119 },
+    { sX: 377, sY: 115, eX: 441, eY: 159, cp1X: 417, cp1Y: 125, cp2X: 403, cp2Y: 146 },
+    { sX: 485, sY: 223, eX: 500, eY: 300, cp1X: 506, cp1Y: 259, cp2X: 481, cp2Y: 264 },
+
+
+    { sX: 441, sY: 441, eX: 377, eY: 485, cp1X: 403, cp1Y: 454, cp2X: 417, cp2Y: 475 },
+    { sX: 300, sY: 500, eX: 223, eY: 485, cp1X: 264, cp1Y: 481, cp2X: 259, cp2Y: 506 },
+    { sX: 159, sY: 441, eX: 115, eY: 377, cp1X: 146, cp1Y: 403, cp2X: 125, cp2Y: 417 },
+    { sX: 100, sY: 300, eX: 115, eY: 223, cp1X: 119, cp1Y: 264, cp2X: 94, cp2Y: 259 },
+    { sX: 159, sY: 159, eX: 223, eY: 115, cp1X: 197, cp1Y: 146, cp2X: 183, cp2Y: 125 },
+    { sX: 300, sY: 100, eX: 377, eY: 115, cp1X: 336, cp1Y: 119, cp2X: 341, cp2Y: 94 },
+    { sX: 441, sY: 159, eX: 485, eY: 223, cp1X: 454, cp1Y: 197, cp2X: 475, cp2Y: 183 },
+    { sX: 500, sY: 300, eX: 485, eY: 377, cp1X: 481, cp1Y: 336, cp2X: 506, cp2Y: 341 },
+
+
+]
