0f7efec8e4b0c0b5196d7302601246f8a4939d6c
NenadVukojevic <nenad.vukojevic@gmail.com>
2024-10-18 06:53:46 +0200

    interactive adjustment of the pearl

--- generators/border_pearl.js
+++ generators/border_pearl.js
@@ -11,25 +11,32 @@
 function generateShapes(centerX, centerY, outer, size) {
     const shapesArray = [];
 
-    shapesArray.push( { x: centerX, y: centerY, radius: outer, startAngle: deg2rad(0), endAngle: deg2rad(365) },);
-    shapesArray.push( { x: centerX, y: centerY, radius: outer -4, startAngle: deg2rad(0), endAngle: deg2rad(365) },);
+    shapesArray.push({ x: centerX, y: centerY, radius: outer, startAngle: deg2rad(0), endAngle: deg2rad(365) },);
+    shapesArray.push({ x: centerX, y: centerY, radius: outer - 4, startAngle: deg2rad(0), endAngle: deg2rad(365) },);
 
-    shapesArray.push( { x: centerX, y: centerY, radius: outer -size + 4, startAngle: deg2rad(0), endAngle: deg2rad(365) },);
-    shapesArray.push( { x: centerX, y: centerY, radius: outer -size, startAngle: deg2rad(0), endAngle: deg2rad(365) },);
+    shapesArray.push({ x: centerX, y: centerY, radius: outer - size + 4, startAngle: deg2rad(0), endAngle: deg2rad(365) },);
+    shapesArray.push({ x: centerX, y: centerY, radius: outer - size, startAngle: deg2rad(0), endAngle: deg2rad(365) },);
 
+    var bRadius = Math.round(outer - size / 2);
+    var pearlSize = Math.round((size - 14) / 2);
 
-    for (let i = 0; i < 120; i++) {
-        const coordinate= getCoordinates(centerX, centerY, Math.round(outer - size/2), i*3);
-        shapesArray.push( { x: coordinate.x, y: coordinate.y, radius: 3, startAngle: deg2rad(0), endAngle: deg2rad(365) },);
+    var circumference = 2 * Math.PI * bRadius;
+
+    var pearlCount = Math.round(circumference / (2 * pearlSize))
+    var pearlAngle = 360 / pearlCount;
+
+    for (let i = 0; i < pearlCount; i++) {
+        const coordinate = getCoordinates(centerX, centerY, bRadius, i * pearlAngle);
+        shapesArray.push({ x: coordinate.x, y: coordinate.y, radius: pearlSize, startAngle: deg2rad(0), endAngle: deg2rad(365) },);
     }
 
     return shapesArray;
 }
 
-function getCoordinates(centerX, centerY, radius, angle){
+function getCoordinates(centerX, centerY, radius, angle) {
     const coordinate = {
-        x: Math.round(centerX + radius*Math.cos(deg2rad(angle))),
-        y: Math.round(centerY + radius*Math.sin(deg2rad(angle))
+        x: Math.round(centerX + radius * Math.cos(deg2rad(angle))),
+        y: Math.round(centerY + radius * Math.sin(deg2rad(angle))
         )
     };
 
